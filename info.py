87348b470f605b86acef51a0e387a8e827bca13a
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2024-07-19 23:40:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,11 +17,11 @@ API_ID = int(environ.get('API_ID', '13305226'))
 API_HASH = environ.get('API_HASH', '8cde2475d6b0cb1162b89ebbac71a95d')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7117702689:AAGD6HDp9GY4E9hsRaGZulPlQHGrOF2OU80')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6497757690').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1258310642 6577722992').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/vis_hnu_bot")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001906470657'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1258310642 6577722992').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002228120220').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://V:V@cluster0.ommvnlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
