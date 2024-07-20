9f6d9190432a3482a19a40d38ce457dff169b4e3
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2024-07-21 00:03:31 +0530

    Update botHandeling.py

--- plugins/components/botHandeling.py
+++ plugins/components/botHandeling.py
@@ -90,7 +90,7 @@ async def m_grp(client, message):
     links = []
     link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is bisal_files")
     if link.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/ASIANET_SURYA_MAZHAVILMANORAMA")
     else:
         if checkIfLinkIsValid(link.text):
             links.append(link.text)
@@ -98,7 +98,7 @@ async def m_grp(client, message):
             await message.reply("Invalid link")
     link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is bisal_files")
     if link1.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/ASIANET_SURYA_MAZHAVILMANORAMA")
     else:
         if checkIfLinkIsValid(link1.text):
             links.append(link1.text)
